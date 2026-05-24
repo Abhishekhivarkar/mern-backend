8a6f0e0b4077fdc578e0a4f65cdf55ef7ba90ba9
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-05-24 16:24:40 +0530

    Delete test-db.js

--- test-db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-await mongoose.connect(
-"mongodb+srv://abhishek783:abhiadi2004@cluster0.0kreanc.mongodb.net/mern-backend?retryWrites=true&w=majority"
-)
-
-console.log(
-"connected"
-)
