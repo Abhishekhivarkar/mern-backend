e64ba8dee9ed9891cf27e17a9f9aad6f405353a0
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-05-27 17:11:01 +0530

    fix(worker): initialize database before starting workers

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,17 @@
-import "./jobs/workers/email.worker.js"
-import "./jobs/workers/notification.worker.js"
-console.log("Email worker started")
-console.log("notification worker started")
+import { connectDB } from "./configs/db.config.js"
+
+export const startWorker = async() =>{
+    try{
+        await connectDB()
+
+        console.log("worker DB start")
+
+        await import("./jobs/workers/email.worker.js")
+        await import("./jobs/workers/notification.worker.js")
+    }catch(err){
+        console.log("Worker startup fialed")
+        console.error(err)
+    }
+}
+
+startWorker()
