3106ac3ce7599306b9856c2b1c4cd47b9c5b668d
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-05-29 13:15:14 +0530

    feat(auth): integrate request validation into auth routes

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import {register,login, logout, refreshToken} from "../controllers/auth.controller.js"
 import { authLimitMiddleware } from "../../../common/middlewares/rateLimit.middleware.js"
-
+import { validate } from "../../../common/middlewares/validation.middleware.js"
+import { registerSchema } from "../validations/auth.validation.js"
 const router = express.Router()
 
-router.post("/register",register)
+router.post("/register",validate({body:registerSchema}),register)
 router.post("/login",authLimitMiddleware,login)
 router.post("/logout",logout)
 router.post("/refresh-token",refreshToken)
