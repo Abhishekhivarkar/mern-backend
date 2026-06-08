14a7e97fbe2f2b56aaa1fc907e48aa90377f0bc8
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-08 17:00:11 +0530

    feat(auth): add password visibility toggle to login form

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -1,52 +1,77 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { useLogin } from "../hooks/useLogin"
-import { useAuthStore } from "../store/auth.store"
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
+import { useAuthStore } from "../store/auth.store";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 function LoginForm() {
-    const setUser = useAuthStore(
-        state => state.setUser
-    )
-    const loginMutation = useLogin()
-    const navigate = useNavigate()
-    const [email,setEmail] = useState("")
-     const [password,setPassword] = useState("")
-     console.log(useLogin)
-console.log("inside handle submit")
-    const handleSubmit = (e:React.FormEvent) =>{
-        console.log("handle clicked")
-        e.preventDefault()
+  const setUser = useAuthStore((state) => state.setUser);
+  const loginMutation = useLogin();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  console.log(useLogin);
+  console.log("inside handle submit");
+  const handleSubmit = (e: React.FormEvent) => {
+    console.log("handle clicked");
+    e.preventDefault();
 
-        loginMutation.mutate({
-            email,password
+    loginMutation.mutate(
+      {
+        email,
+        password,
+      },
+      {
+        onSuccess: (data) => {
+          setUser(data.data);
+
+          navigate("/");
         },
-    {
-        onSuccess:(data) => {
-            
-            setUser(data.data)
-            
-            navigate("/")
-        }
-    })
-    }
+      },
+    );
+  };
   return (
     <div className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2  rounded-lg p-2 bg-gray-700"
+      >
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          className="border rounded p-1"
+        />
 
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2  rounded-lg p-2 bg-gray-700">
-            
-            <input type="email" name="email" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)} className="border rounded p-1"/>
-
-           
-            <input type="password" name="password" value={password} placeholder="Password"  onChange={(e)=>setPassword(e.target.value)} className="border rounded p-1" />
-            
-            <button type="submit" className="bg-gray-400 rounded hover:bg-blue-500 tranisition-transform duration-150 active:scale-98">
-              Enter
-            </button>
-        </form>
-
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="border rounded p-1 "
+          />
+          <button
+            className="absolute right-3 top-1/2 -translate-y-1/2"
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <FaEye /> : <FaEyeSlash />}
+          </button>
+        </div>
+        <button
+          type="submit"
+          className="bg-gray-400 rounded hover:bg-blue-500 tranisition-transform duration-150 active:scale-98"
+        >
+          Enter
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
