e346c607a9b739af0fb3f777effbf12dd1f4edfa
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-09 12:42:37 +0530

    feat(notes): add payment and purchased notes routes

--- backend/src/modules/notes/routes/notes.routes.ts
+++ backend/src/modules/notes/routes/notes.routes.ts
@@ -1,4 +1,4 @@
- import {createNotes,getAllNotes,patchUpdateNotes,deleteNotes,pinNotes ,unPinNotes,getPinnedNotes, getMyNotes,createNotePurchase, createNoteOrder} from "../controllers/notes.controller.js"
+ import {createNotes,getAllNotes,patchUpdateNotes,deleteNotes,pinNotes ,unPinNotes,getPinnedNotes, getMyNotes, createNoteOrder, verifyPyament, getPurchasedNotes, getNoteById} from "../controllers/notes.controller.js"
 import express from "express"
 import {authMiddleware} from "../../../common/middlewares/auth.middleware.js"
 import { validate } from "../../../common/middlewares/validation.middleware.js"
@@ -9,7 +9,7 @@ const router = express.Router()
 router.post("/create",authMiddleware,validate({body:createNotesSchema}),createNotes)
 
 
-router.get("/all",authMiddleware,getAllNotes)
+router.get("/all",getAllNotes)
 
 
 router.patch("/:note_id",authMiddleware,validate({
@@ -27,9 +27,15 @@ router.patch("/un-pin/:note_id",authMiddleware,unPinNotes)
 router.get("/pin",authMiddleware,getPinnedNotes)
 router.get("/my",authMiddleware,getMyNotes)
 
-router.post("/:note_id/purchase",authMiddleware,createNotePurchase)
+// router.post("/:note_id/purchase",authMiddleware,createNotePurchase)
 
 router.post("/:note_id/order",authMiddleware,createNoteOrder)
+
+router.post("/:note_id/verify",authMiddleware,verifyPyament)
+
+router.get("/purchased-notes",authMiddleware,getPurchasedNotes)
+
+router.get("/:note_id",authMiddleware,getNoteById)
 export default router
 
 
