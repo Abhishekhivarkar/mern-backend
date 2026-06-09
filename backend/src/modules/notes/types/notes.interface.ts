7801bf1000ffbe91dd75aa693e98bfe85be7096c
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-09 12:42:36 +0530

    feat(notes): add note purchase and payment interfaces

--- backend/src/modules/notes/types/notes.interface.ts
+++ backend/src/modules/notes/types/notes.interface.ts
@@ -4,6 +4,9 @@ export interface INotes extends Document{
  title:string,
  content:string,
  user:mongoose.Types.ObjectId,
+ price:number,
+ is_paid:boolean
+ is_published:boolean
  isDeleted:boolean,
  createdAt:Date,
  updatedAt:Date
