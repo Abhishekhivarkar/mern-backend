72f4d41e9999bbdc5e01f6974b877945f935b145
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-10 18:27:01 +0530

    style(auth): redesign register form with icons

--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -2,52 +2,100 @@ import { useState } from "react";
 
 import { useRegister } from "../hooks/useRegister";
 import { useNavigate } from "react-router-dom";
-
+import { FiUser } from "react-icons/fi";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 export const RegisterForm = () => {
-  const navigate = useNavigate();
-  const registerMutation = useRegister();
 
+  const registerMutation = useRegister()
+const navigate = useNavigate()
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    registerMutation.mutate(
-      {
-        email,
-
-        password,
-      },
-      {
-        onSuccess: () => {
-          navigate("/login");
-        },
-      },
-    );
+    registerMutation.mutate({
+      email,password
+    },
+  {
+    onSuccess:()=>{
+      navigate("/login")
+    }
+  })
+    
   };
 
   return (
-    <div className="flex justify-center items-center h-screen ">
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2 bg-gray-700  rounded-lg p-2 ">
-      <input 
-      className="border rounded p-1"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="h-screen flex items-center justify-center  "
+      >
+        <div className="border rounded-xl  p-5 bg-white/20 backdrop-blur-xl border border-white/30 shadow-xl">
+          <div className="flex flex-col items-center mb-4">
+            <div className="py-1.5 px-1.5 rounded-full bg-blue-200 text-blue-700">
+              <FiUser />
+            </div>
 
-      <input
-      className="border rounded p-1"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
+            <p className="text-[1rem] font-bold">Create Account</p>
+            <p className="text-[9px] font-sm">join us and get started</p>
+          </div>
+          <div className="flex flex-col font-semibold gap-4">
+            <div className="flex flex-col ">
+              <label htmlFor="email">Email</label>
+              <div className="relative">
+                <MdOutlineEmail className="absolute left-2 top-1/2 -translate-y-1/2" />
 
-      <button className="bg-gray-400 rounded hover:bg-blue-500 tranisition-transform duration-150 active:scale-98"type="submit">Register</button>
-    </form>
-    </div>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Enter your email"
+                  className="border border-gray-300 rounded-full pl-8 py-1 outline-none"
+                  onChange={e=>setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="password">Password</label>
+
+              <div className="relative">
+                <RiLockPasswordLine className="absolute -translate-y-1/2 top-1/2 left-2" />
+                <input
+                  type={show ? "text" : "password"}
+                  name="password"
+                  value={password}
+                  placeholder="Enter your password"
+                  className="pl-8 rounded-full py-1 border border-gray-300 outline-none"
+                  onChange={e=>setPassword(e.target.value)}
+                />
+
+                <button
+                  onClick={() => setShow(!show)}
+                  className="absolute top-1/2 -translate-y-1/2 right-2"
+                >
+                  {show ? <FaEye /> : <FaEyeSlash />}
+                </button>
+              </div>
+            </div>
+
+            <input
+              type="submit"
+              className="rounded px-1 py-1 bg-blue-600 text-white"
+            />
+            <p className="text-[9px] text-center">
+              Already have an account?{" "}
+              <a href="/login" className="text-blue-600 font-bold">
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </form>
+    </>
   );
 };
