2e4ec1a17231cd8caf96d84f293118f6e37d869b
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-10 14:47:12 +0530

    feat(home): add homepage pages

---
+++ frontend/src/features/home/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+import { LogoutButton } from '../../auth/components/LogoutButton'
+import { Navbar } from '../../../common/components/Navbar'
+import { HeroSection } from '../components/HeroSection'
+import { CategoriesSection } from '../components/CategoriesSection'
+
+export const Dashboard = ()  => {
+  return (
+  
+   <>
+   <Navbar/>
+   <HeroSection/>
+   <CategoriesSection/>
+   </>
+  )
+}
