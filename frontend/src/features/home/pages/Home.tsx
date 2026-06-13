164b997574d43400d5026a974444b77c7b408ae3
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-13 12:03:03 +0530

    feat(home): compose homepage layout

--- frontend/src/features/home/pages/Home.tsx
+++ frontend/src/features/home/pages/Home.tsx
@@ -5,16 +5,23 @@ import { HeroSection } from '../components/HeroSection'
 import { CategoriesSection } from '../components/CategoriesSection'
 import FeaturedNotesSection from '../components/FeaturedNotesSection'
 import WhyChooseUsSection from '../components/WhyChooseUsSection'
+import SellerSection from '../components/SellerSection'
+import Footer from '../../../common/components/Footer'
 
 export const Dashboard = ()  => {
   return (
   
    <>
-   <Navbar/>
+   <div className='flex flex-col gap-7 '>
+      <Navbar/>
    <HeroSection/>
    <CategoriesSection/>
    <FeaturedNotesSection/>
    <WhyChooseUsSection/>
+  <SellerSection/>
+  <Footer />
+   </div>
+ 
    </>
   )
 }
