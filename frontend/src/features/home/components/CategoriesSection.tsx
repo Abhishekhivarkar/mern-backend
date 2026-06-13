44f4cba31538b10782cd541dedbd7af9177c87a8
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-13 12:03:02 +0530

    feat(home): add categories showcase section

--- frontend/src/features/home/components/CategoriesSection.tsx
+++ frontend/src/features/home/components/CategoriesSection.tsx
@@ -35,6 +35,27 @@ const getCategoryLogo = (category: string) => {
   }
 };
 
+const categoryName = (category: string) => {
+  switch (category) {
+    case "PROGRAMMING":
+      return "Programming";
+
+    case "DATA_STRUCTURE":
+      return "Data Structure";
+
+    case "COLLEGE_NOTES":
+      return "College Notes";
+
+    case "WEB_DEVELOPMENT":
+      return "Web Development";
+
+    case "DATABASE":
+      return "Database";
+
+    default:
+      return "View All";
+  }
+};
 const getColorClass = (category: string) => {
   switch (category) {
     case "PROGRAMMING":
@@ -64,7 +85,7 @@ export const CategoriesSection = () => {
   }
 
   return (
-    <div className="bg-white mt-4 px-15">
+    <div className="bg-white px-15">
       <div className="flex flex-col items-center mb-3">
         <h1 className="text-black  font-bold">Popular Categories</h1>
         <div className="text-violet-500  rounded font-bold ">{"_______"}</div>
@@ -76,6 +97,7 @@ export const CategoriesSection = () => {
           return (
             <>
               <div
+              data-aos="fade-up"
                 className="flex flex-col justify-center items-center bg-gray-50 py-4 shadow gap-2"
                 key={i.id}
               >
@@ -84,13 +106,15 @@ export const CategoriesSection = () => {
                 >
                   {getCategoryLogo(i.category)}
                 </div>
-                <p className="font-bold text-sm ">{i.category}</p>
+                <p className="font-bold text-sm ">{categoryName(i.category)}</p>
                 <p>{i.count}</p>
               </div>
             </>
           );
         })}
-         <div className="flex flex-col justify-center items-center bg-gray-50 py-4 shadow cursor-pointer">
+         <div 
+         data-aos="fade-up"
+         className="flex flex-col justify-center items-center bg-gray-50 py-4 shadow cursor-pointer gap-2">
     <div className="rounded py-2 px-2 text-white bg-violet-500">
       <CgMenuGridO />
     </div>
