d3cfde9d6aa0651d0cd96527526fba1140e9ebf8
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-13 12:03:02 +0530

    feat(ui): add reusable brand logo component

---
+++ frontend/src/common/components/BrandLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BrandLogo() {
+  return (
+    <div className="font-bold">
+        <p>
+          Note<span className="text-violet-500">Hub</span>
+        </p>
+      </div>
+  )
+}
