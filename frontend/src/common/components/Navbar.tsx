68c455217cfbbeb52affe046312f209403b925bc
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-14 17:07:28 +0530

    feat(ui): update navbar navigation for notes pages

--- frontend/src/common/components/Navbar.tsx
+++ frontend/src/common/components/Navbar.tsx
@@ -7,11 +7,12 @@ export const Navbar = () => {
 
   return (
     <>
-<nav className="bg-[#fefefe] text-black shadow-lg">
-  <div className="flex justify-between items-center w-full p-4">
+<nav className="bg-[#fefefe] text-black shadow-lg fixed w-full top-0 left-0 z-50">
+  <div className="flex justify-between items-center w-full p-4 ">
     {/* logo div */}
     <div className="flex justify-between items-center w-full md:w-auto">
-      <BrandLogo/>
+      <a href="/"> <BrandLogo /></a>
+     
 
 
       <button
@@ -24,12 +25,12 @@ export const Navbar = () => {
 <div className="flex justify-between items-center gap-9">
     {/* desktop menu div */}
     <div className="hidden md:block">
-      <ul className="flex gap-6">
-        <li>Notes</li>
-        <li>Category</li>
-        <li>Become Seller</li>
-        <li>Pricing</li>
-      </ul>
+      <div className="flex gap-6">
+        <a href="/notes/all">Notes</a>
+        <a>Category</a>
+        <a>Become Seller</a>
+        <a>Pricing</a>
+      </div>
     </div>
 
     {/* desktop search div */}
@@ -40,23 +41,28 @@ export const Navbar = () => {
     
   </div>
 </div>
-  {isOpen && (
-    <div className="md:hidden pl-4 pt-3 pr-4 pb-4">
+  
+    <div className={`md:hidden pl-4 pt-3 pr-4 pb-4 absolute bg-white w-full transition-all duration-500 ease-in-out
+      
+      ${
+        isOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-4 pointer-events-none"
+      }
+    `}>
     
 
       <div className="font-medium">
-        <ul className="text-center">
-          <li>Notes</li>
-          <li>Category</li>
-          <li>Become seller</li>
-          <li>Pricing</li>
-        </ul>
+        <div className="flex flex-col text-center">
+          <a href="/notes/all">Notes</a>
+          <a>Category</a>
+          <a>Become seller</a>
+          <a>Pricing</a>
+        </div>
       </div>
       <div className="pt-2 flex justify-center font-bold">
         <LogoutButton />
       </div>
     </div>
-  )}
+  
 </nav>
       </>
   );
