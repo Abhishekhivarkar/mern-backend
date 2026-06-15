03749559fe5057881aa026d55334f008c48d7d6d
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-15 16:39:58 +0530

    feat(notes): build notes listing page layout

--- frontend/src/features/notes/pages/GetAllNotesPage.tsx
+++ frontend/src/features/notes/pages/GetAllNotesPage.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../../common/components/Footer";
 import { Navbar } from "../../../common/components/Navbar";
-import NotesFilterSection from "../components/NotesFilterSection";
+import AllNotesSection from "../components/AllNotesSection";
+import NotesFilterSection from "../components/NotesFilterSidebar";
 
 export default function GetAllNotesPage() {
   return (
@@ -8,8 +9,9 @@ export default function GetAllNotesPage() {
       <div className="min-h-screen flex flex-col gap-7">
         <Navbar/>
           <main className="flex-1">
-            <div className="mt-20">
+            <div className="mt-20 px-10 flex gap-12">
               <NotesFilterSection/>
+              <AllNotesSection/>
             </div>
               
           </main>
