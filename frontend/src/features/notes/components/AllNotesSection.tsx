49e4f52e5088aade0ba6447bc3b5dc4f0e53465a
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-16 22:27:02 +0530

    feat(notes): improve notes catalog section layout

--- frontend/src/features/notes/components/AllNotesSection.tsx
+++ frontend/src/features/notes/components/AllNotesSection.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeNavigationIcon from './HomeNavigationIcon'
 import AllNotesSearchBar from './AllNotesSearchBar'
+import NotesCatalog from './NotesCatalog'
 
 export default function AllNotesSection() {
   return (
-    <div className=' border-1 border-gray-200  w-full'>
+    <div className=' border border-gray-200  w-full'>
         <HomeNavigationIcon/>
         <AllNotesSearchBar/>
+        <NotesCatalog/>
     </div>
   )
 }
