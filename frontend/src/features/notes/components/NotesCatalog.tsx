fd9cdb6b219965f310fa66f7061a9998043c036e
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-17 11:25:28 +0530

    feat(notes): add notes catalog grid layout and listing

--- frontend/src/features/notes/components/NotesCatalog.tsx
+++ frontend/src/features/notes/components/NotesCatalog.tsx
@@ -1,17 +1,20 @@
-import { useGetNotes } from "../hooks/useGetNotes"
+import { useGetNotes } from "../hooks/useGetNotes";
+import NotesCard from "./NotesCard";
 
 export default function NotesCatalog() {
-
-    const {data} = useGetNotes()
+  const { data } = useGetNotes();
   return (
     <div>
       <p>Showing 1 to 12 of 248 notes</p>
-
-      <div>
-        {
-            data?.data?.notes?.map()
-        }
+      <div className="grid grid-cols-4 gap-9">
+        {data?.data?.notes?.map((i) => {
+          return (
+            <>
+              <NotesCard data={i} />
+            </>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
