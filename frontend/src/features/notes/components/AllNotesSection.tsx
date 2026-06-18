75bd143e50ef83acb7ea5a030af0efbb2b43b2e9
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-18 19:39:25 +0530

    feat(notes): refine notes listing section

--- frontend/src/features/notes/components/AllNotesSection.tsx
+++ frontend/src/features/notes/components/AllNotesSection.tsx
@@ -4,14 +4,16 @@ import AllNotesSearchBar from './AllNotesSearchBar'
 import NotesCatalog from './NotesCatalog'
 
 interface Props{
-  category:string
+  category:string,
+  minPrice:number,
+  maxPrice:number
 }
-export default function AllNotesSection({category}:Props) {
+export default function AllNotesSection({category,minPrice,maxPrice}:Props) {
   return (
     <div className=' border border-gray-200  w-full'>
         <HomeNavigationIcon/>
         <AllNotesSearchBar/>
-        <NotesCatalog category={category}/>
+        <NotesCatalog category={category} minPrice={minPrice} maxPrice={maxPrice}/>
     </div>
   )
 }
