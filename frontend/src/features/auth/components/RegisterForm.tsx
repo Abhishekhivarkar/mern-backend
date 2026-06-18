394b03b74dd80a0384484b3d2aa6fc22b7ef9c5d
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-18 19:39:24 +0530

    feat(auth): enhance registration form handling

--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -34,7 +34,7 @@ const navigate = useNavigate()
         onSubmit={handleSubmit}
         className="h-screen flex items-center justify-center  "
       >
-        <div className="border rounded-xl  p-5 bg-white/20 backdrop-blur-xl border border-white/30 shadow-xl">
+        <div className="border rounded-xl  p-5 bg-white/20 backdrop-blur-xl  border-white/30  shadow-[0_0_20px_rgba(0,0,0,0.20)]">
           <div className="flex flex-col items-center mb-4">
             <div className="py-1.5 px-1.5 rounded-full bg-blue-200 text-blue-700">
               <FiUser />
