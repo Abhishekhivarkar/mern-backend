ce8ec7e89c68e34e24eb3a63825a5d90adf2520e
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-18 15:12:20 +0530

    feat(notes): integrate notes filters with listing page

--- frontend/src/features/notes/pages/AllNotesPage.tsx
+++ frontend/src/features/notes/pages/AllNotesPage.tsx
@@ -1,22 +1,26 @@
+import { useState } from "react";
 import Footer from "../../../common/components/Footer";
 import { Navbar } from "../../../common/components/Navbar";
 import AllNotesSection from "../components/AllNotesSection";
 import NotesFilterSection from "../components/NotesFilterSidebar";
 
 export default function GetAllNotesPage() {
+  const [category, setCategory] = useState("");
   return (
     <div>
       <div className="min-h-screen flex flex-col gap-7">
-        <Navbar/>
-          <main className="flex-1">
-            <div className="mt-20 px-10 flex gap-12">
-              <NotesFilterSection/>
-              <AllNotesSection/>
-            </div>
-              
-          </main>
-        <Footer/>
-        </div>
+        <Navbar />
+        <main className="flex-1">
+          <div className="mt-20 px-10 flex gap-12">
+            <NotesFilterSection
+              selectedCategory={category}
+              onCategoryChange={setCategory}
+            />
+            <AllNotesSection category={category} />
+          </div>
+        </main>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
