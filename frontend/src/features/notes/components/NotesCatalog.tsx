1942dc8c701dd27c8e7c4b1bcb9c2e2832f75263
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-18 19:39:25 +0530

    feat(notes): improve notes catalog filtering logic

--- frontend/src/features/notes/components/NotesCatalog.tsx
+++ frontend/src/features/notes/components/NotesCatalog.tsx
@@ -1,35 +1,67 @@
+import { useState } from "react";
 import { useGetNotes } from "../hooks/useGetNotes";
 import NotesCard from "./NotesCard";
 
-interface Props{
-    category:string
-} 
+interface Props {
+  category: string;
+  minPrice:number,
+  maxPrice:number
+}
 
-export default function NotesCatalog({category}:Props) { 
-  
-  
+export default function NotesCatalog({ category,minPrice,maxPrice }: Props) {
+  const [page, setPage] = useState(1);
+  const limit = 8;
+
   const { data } = useGetNotes({
-    page:1,
-    limit:0,
-    category:category || undefined
+    page,
+    limit,
+    category: category || undefined,
+    minPrice: minPrice || undefined,
+    maxPrice: maxPrice || undefined
   });
 
-  const { page , limit, total } = data?.data || {};
+  const { total = 0 } = data?.data || {};
 
-  const start = total ? (page - 1) * limit + 1 : 0;
-  const end = Math.min(page * limit, total);
+  const totalPages = Math.ceil(total / limit);
+const notes = data?.data?.notes || [];
 
   return (
-    <div className="mt-3">
-      <p>
-        Showing {start} to {end} of {total} notes
+   <div className="mt-3">
+  {notes.length === 0 ? (
+    <div className="flex justify-center items-center h-40">
+      <p className="text-gray-500 text-lg">
+        Notes not found for this search.
       </p>
-
+    </div>
+  ) : (
+    <>
       <div className="grid grid-cols-4 gap-9">
-        {data?.data?.notes?.map((note) => (
+        {notes.map((note) => (
           <NotesCard key={note.note_id} data={note} />
         ))}
       </div>
-    </div>
+
+      <div className="flex gap-4 mt-6">
+        <button
+          disabled={page === 1}
+          onClick={() => setPage((prev) => prev - 1)}
+        >
+          Previous
+        </button>
+
+        <span>
+          Page {page} of {totalPages}
+        </span>
+
+        <button
+          disabled={page === totalPages}
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  )}
+</div>
   );
 }
