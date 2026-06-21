c4d51c98dd95c242678e678dd8dd92f0cd9166f2
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-21 12:21:12 +0530

    fix: update notification service types

--- backend/src/common/services/notification.service.ts
+++ backend/src/common/services/notification.service.ts
@@ -1,17 +1,33 @@
 import { notificationQueue } from "../../jobs/queues/notification.queue.js";
 
-export const sendNoteCreatedNotificationToUser = async(user_id:string,payload:{
-    note_id:string,
-    title:string,
-    message:string
-}) =>{
-    await notificationQueue.add("note-created",{user_id:user_id,type:"create",event:"note:created",payload})
-}
+export const sendNoteCreatedNotificationToUser = async (
+  user_id: string,
+  payload: {
+    note_id: string;
+    title: string;
+    message: string;
+  },
+) => {
+  await notificationQueue.add("note-created", {
+    user_id: user_id,
+    type: "create",
+    event: "note:created",
+    payload,
+  });
+};
 
-export const sendNoteUpdatedNotificationToUser = async(user_id:string,payload:{
-    title:string | undefined,note_id:string,message:string
-}) =>{
-    await notificationQueue.add("note-update",{
-        user_id,event:"note:update",payload
-    })
-}
+export const sendNoteUpdatedNotificationToUser = async (
+  user_id: string,
+  payload: {
+    title: string | undefined;
+    note_id: string;
+    message: string;
+  },
+) => {
+  await notificationQueue.add("note-update", {
+    user_id:user_id,
+    type:"update",
+    event: "note:update",
+    payload,
+  });
+};
